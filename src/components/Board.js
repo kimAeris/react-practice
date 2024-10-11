95b0a9049a68286347ff5b75d2ee98218df5e304
Aeris <kar2125@naver.com>
2024-10-11 20:43:10 +0900

    승자 결정시 return

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,10 +6,49 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
-  const status = `Next player ${xIsNext ? "X" : "O"}`;
+  const calculateWinner = (squares) => {
+    // 이기는 경우의 수
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 4, 8],
+      [0, 4, 8],
+      [2, 4, 9],
+    ];
+
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  };
+
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player ${xIsNext ? "X" : "O"}`;
+  }
 
   const handleClick = (i) => {
     const newSquares = squares.slice();
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+
     newSquares[i] = xIsNext ? "X" : "O";
     setSquares(newSquares);
     setXIsNext((prev) => !prev);
