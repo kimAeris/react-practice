f8cd3db2d3d5b420ea9ed96cb95b907e2b635f56
kimAeris <kar2125@naver.com>
2024-11-06 12:05:23 +0900

    스텝, SummaryPage

--- client/src/pages/OrderPage/index.js
+++ client/src/pages/OrderPage/index.js
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
 import Type from "../../components/Type";
-// import { OrderContext } from "../../context/OrderContext";
+import { OrderContext } from "../../context/OrderContext";
 
 const OrderPage = ({ setStep }) => {
-  // const [orderData] = useContext(OrderContext);
+  const [orderData] = useContext(OrderContext);
   return (
     <div>
       <h1>Travel Products</h1>
@@ -15,8 +15,8 @@ const OrderPage = ({ setStep }) => {
           <Type orderType="options" />
         </div>
         <div style={{ width: "50%" }}>
-          <h2>Total Price: </h2>
-          <button>주문</button>
+          <h2>Total Price: {orderData.totals.total} </h2>
+          <button onClick={() => setStep(1)}>주문</button>
         </div>
       </div>
     </div>
