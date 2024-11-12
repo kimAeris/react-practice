7a885c234eebcfc8e1b2644879e12a422e30d4a1
KimAeris <kar2125@naver.com>
2024-11-12 15:37:27 +0900

    admin , user 라우터 생성

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminPage = () => {
+  return <div>Admin View</div>;
+};
+
+export default AdminPage;
---
+++ src/app/user/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserPage = () => {
+  return <div>Logined View</div>;
+};
+
+export default UserPage;
