f0817883e54964b29b453878da757d4a9d08b8dd
KimAeris <kar2125@naver.com>
2024-11-15 01:18:47 +0900

    상품 상세페이지

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getProductById from "@/app/actions/getProductById";
+import EmptyState from "@/components/EmptyState";
+import ProductClient from "./ProductClient";
+interface Params {
+  productId?: string;
+}
 
-const ProductPage = () => {
-  return <div>ProductPage</div>;
+const ProductPage = async ({ params }: { params: Params }) => {
+  const product = await getProductById(params);
+  const currentUser = await getCurrentUser();
+
+  console.log("product", product);
+
+  if (!product) {
+    return <EmptyState />;
+  }
+
+  return <ProductClient product={product} currentUser={currentUser} />;
 };
 
 export default ProductPage;
